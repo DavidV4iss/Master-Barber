627b3663fcb9afc6fd7e7800cf49ee64e449ead1
FidelSmoke <146258944+FidelSmoke@users.noreply.github.com>
2024-07-09 17:47:06 -0500

    fidel
    fidel

---
+++ dashboard-react/dashboard-react/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from '../src/Components/Navbar'
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>HELLO WORLD</h1>
+    </div>
+  );
+}
+
+export default App;
