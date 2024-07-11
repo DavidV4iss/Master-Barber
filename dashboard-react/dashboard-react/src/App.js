157671375ea99ee5cd73de23159f90e0601775c8
FidelSmoke <146258944+FidelSmoke@users.noreply.github.com>
2024-07-11 10:30:57 -0500

    ffffffffff
    fffffffffffffffffff

--- dashboard-react/dashboard-react/src/App.js
+++ dashboard-react/dashboard-react/src/App.js
@@ -1,12 +1,24 @@
-import Navbar from '../src/Components/Navbar'
+import { BrowserRouter as Router, Route, BrowserRouter, Routes } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+import Sidebar from '../src/Components/Sidebar'
+import Home from './pages/Home'
+import Sales from './pages/Sales'
+import Clients from './pages/Clients'
 import './App.scss';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-      <h1>HELLO WORLD</h1>
-    </div>
+      <Sidebar />
+      <Routes>
+        <Route exact path='*' element={<Navbar />}></Route>
+        <Route exact path='/' element={<Home />}> </Route>
+        <Route exact path='/sales' element={<Sales />}> </Route>
+        <Route exact path='/clients' element={<Clients />}> </Route>
+      </Routes>
+    </Router>
+
   );
 }
 
